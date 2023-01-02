70d98699904d554a4135015b5736d0089e6368bc
NicoEberlein <nico@eberln.de>
2023-01-03 00:28:13 +0100

    index page now gets custom team names via rest endpoint /rest/getTeamNames

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,15 +3,22 @@ window.onload = function() {
 }
 
 function sendBuzzerDataRequest() {
-    sendRequest(processBuzzerData, window.location.origin + "/data", "GET", null, new Headers());
+    sendRequest(processBuzzerData, window.location.origin + "/rest/data", "GET", null, new Headers());
+    sendRequest(processTeamNames, window.location.origin + "/rest/getTeamNames", "GET", null, new Headers());
 }
 
 function processBuzzerData(data) {
     if(data != null) {
-        document.getElementById("counter_0").innerHTML = data.data[0];
-        document.getElementById("counter_1").innerHTML = data.data[1];
-        document.getElementById("counter_2").innerHTML = data.data[2];
+        document.getElementById("counter_0").innerHTML = data[0];
+        document.getElementById("counter_1").innerHTML = data[1];
+        document.getElementById("counter_2").innerHTML = data[2];
     }
+}
+
+function processTeamNames(data) {
+	for(var i = 1; i<data.length+1; i++) {
+		document.getElementById("teamname" + i).innerHTML = data[i-1];
+	}
 }
 
 function sendRequest(functionToCall, url, type, body, headers){
