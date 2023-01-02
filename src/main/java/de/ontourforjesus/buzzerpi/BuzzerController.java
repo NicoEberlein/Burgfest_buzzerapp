424fa29ca1de9db84d2b1056fa2decb942dcb669
NicoEberlein <nico@eberln.de>
2023-01-03 00:27:15 +0100

    settings page can be used for adjusting gamemode and team names

--- src/main/java/de/ontourforjesus/buzzerpi/BuzzerController.java
+++ src/main/java/de/ontourforjesus/buzzerpi/BuzzerController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BuzzerController {
-
+	
 	@RequestMapping("/")
 	public String getBuzzerVisualizationPage() {
+		
 		return "index";
+	}
+	
+	@RequestMapping("/settings")
+	public String getBuzzerSettingsPage() {
+		return "settings";
 	}
 	
 }
---
+++ src/main/resources/static/js/settings.js
@@ -0,0 +1,85 @@
+function sendRequest(functionToCall, url, type, body, headers){
+	let request = {
+		method: type,
+		headers: headers,
+		body: body
+	};
+	let responseObj = {
+		responseBody: null,
+		responseStatus: null
+	}
+
+	fetch(url, request)
+		.then(function(response) {
+			if(!response.ok) {
+				throw Error(response.status);
+			}
+			responseObj.responseStatus = response.status;
+			return response.text();
+		})
+		.then(function(text) {
+			if(text.length > 0) {
+				return JSON.parse(text);
+			}else{
+				return null;
+			}
+		})
+		.then(function(response){
+			responseObj.responseBody = response;
+			if(functionToCall != null) {
+				functionToCall(responseObj);	
+			}
+		})
+		.catch(function(error) {
+			console.log(error.message);
+		});
+}
+
+function reset() {
+	sendRequest(null, window.location.origin + "/rest/reset", "GET", null, new Headers());
+}
+
+function showSuccessMessage(responseObj) {
+	if(responseObj.responseStatus == 200) {
+		let success_div = document.createElement("div");
+		success_div.setAttribute("class", "alert alert-success mt-1");
+		success_div.innerHTML = "Daten wurden erfolgreich versendet";
+
+		document.getElementById("status").appendChild(success_div);
+	}
+}
+
+
+function clearErrors() {
+	let errors = document.getElementsByClassName("alert");
+	for(var i=0;i<errors.length;i++) {
+		document.getElementById("status").removeChild(errors[i]);
+	}
+}
+
+function sendFormData() {
+	
+	clearErrors();
+	
+	var body = new FormData(document.getElementById("buzzer-settings-form"));
+	
+	if(body.get("teamname1").length == 0) {
+		body.set("teamname1", "Team 1");
+	}
+	if(body.get("teamname2").length == 0) {
+		body.set("teamname2", "Team 2");
+	}
+	if(body.get("teamname3").length == 0) {
+		body.set("teamname3", "Team 3");
+	}
+	
+	sendRequest(showSuccessMessage, window.location.origin + "/rest/settings", "POST", body, new Headers());
+	
+}
+
+
+
+
+
+
+
