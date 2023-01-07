c83651830b53124b2ecf12c0c737ed3efd743432
NicoEberlein <nico@eberln.de>
2023-01-07 18:16:07 +0100

    javascript FormData entries are now deleted if they contain an empty string

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+	window.setInterval(sendCurrentScoreRequest, 1000);
+}
+
 function sendRequest(functionToCall, url, type, body, headers){
 	let request = {
 		method: type,
@@ -35,9 +39,49 @@ function sendRequest(functionToCall, url, type, body, headers){
 		});
 }
 
+function sendCurrentScoreRequest() {
+	sendRequest(processCurrentScoreResponse, window.location.origin + "/rest/data", "GET", null, new Headers());
+}
+
+function processCurrentScoreResponse(data) {
+	var responseBody = data.responseBody;
+	
+	if(responseBody.gameModeType === "Counter") {
+		
+		for(var i = 0;i<3;i++) {
+			document.getElementById("current-score-" + (i+1)).innerHTML = responseBody.buzzerCounter[i];
+		}
+		
+	}else if(responseBody.gameModeType === "WhoWasFirst") {
+		
+		for(var i = 0; i<3; i++) {
+			document.getElementById("current-score-" + (i+1)).innerHTML = "";
+		}
+		
+		if(responseBody.firstBuzzer != -1) {
+			document.getElementById("current-score-" + (responseBody.firstBuzzer + 1)).innerHTML = "Erster";
+		}
+		
+	}
+}
+
 function resetGameData() {
+	clearErrors();
 	sendRequest(showStatusMessage, window.location.origin + "/rest/reset", "GET", null, new Headers());
 }
+
+function incrementBuzzer(buzzer) {
+	clearErrors();
+	sendRequest(showStatusMessage, window.location.origin + "/rest/incrementBuzzer/" + buzzer, "GET", null, new Headers());
+}
+
+function decrementBuzzer(buzzer) {
+	clearErrors();
+	sendRequest(showStatusMessage, window.location.origin + "/rest/decrementBuzzer/" + buzzer, "GET", null, new Headers());
+}
+
+
+
 
 function showStatusMessage(responseObj) {
 	if(responseObj.responseStatus == 200) {
@@ -69,15 +113,22 @@ function sendFormData() {
 	
 	var body = new FormData(document.getElementById("buzzer-settings-form"));
 	
-	if(body.get("teamname1").length == 0) {
-		body.set("teamname1", "Team 1");
+	var keysToDelete = [];
+	var i = 0;
+	
+	//Delete empty string parameters
+	for(var entry of body) {
+		if(entry[1].length == 0) {
+			keysToDelete[i] = entry[0];
+		}
+		i++;
 	}
-	if(body.get("teamname2").length == 0) {
-		body.set("teamname2", "Team 2");
+	
+	for(var j = 0; j<keysToDelete.length; j++) {
+		body.delete(keysToDelete[j]);
 	}
-	if(body.get("teamname3").length == 0) {
-		body.set("teamname3", "Team 3");
-	}
+	
+	console.log(body);
 	
 	sendRequest(showStatusMessage, window.location.origin + "/rest/settings", "POST", body, new Headers());
 	
